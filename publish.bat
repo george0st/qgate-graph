2694a7270d259eae714db3889700c27006f91e2a
jist <95856749+george0st@users.noreply.github.com>
2023-10-20 20:18:20 +0200

    Deployment based on toml

--- publish.bat
+++ publish.bat
@@ -1,7 +1,23 @@
-rem pip install --upgrade setuptools wheel
+rem pip install --upgrade build
 rem pip install --upgrade twine
 
 rmdir /S /Q dist
+rmdir /S /Q build
+rmdir /S /Q qgate_graph.egg-info
 
-python setup.py bdist_wheel
-twine upload dist/* --verbose -ugeorge0st
+rem 'setup.py bdist_wheel' is deprecated recommendation is to use 'python -m build'
+rem python -m build --wheel
+rem py -m build
+
+rem helper 'https://www.scivision.dev/python-minimal-package/'
+rem https://pypa-build.readthedocs.io/en/latest/
+rem https://github.com/pypa/build
+python -m build --sdist
+
+rem  --no-isolation
+rem --sdist, -s - build a source distribution (disables the default behavior)
+rem --wheel, -w - build a wheel (disables the default behavior)
+
+
+rem twine upload is supported
+twine upload dist/* --verbose -u__token__
