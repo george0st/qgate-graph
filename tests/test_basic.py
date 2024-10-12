044594d0570c947b086e5fa89232586279f80322
jist <95856749+george0st@users.noreply.github.com>
2024-10-12 22:54:01 +0200

    Update test_basic.py

--- tests/test_basic.py
+++ tests/test_basic.py
@@ -87,7 +87,7 @@ class TestCaseBasic(unittest.TestCase):
         graph = GraphExecutor()
         output=graph.generate_from_dir(TestCaseBasic.INPUT_ADR, self.OUTPUT_ADR)
 
-        self.assertTrue(len(output)==47)
+        self.assertTrue(len(output)==65)
 
     def test_check_path(self):
         """Check, dir with duration and date"""
