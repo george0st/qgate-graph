72490f40857ca415ccd3df82d119126ebdd1a5b7
jist <95856749+george0st@users.noreply.github.com>
2024-10-14 21:23:56 +0200

    ...

--- qgate_graph/graph_txt.py
+++ qgate_graph/graph_txt.py
@@ -10,37 +10,5 @@ class GraphTxt(GraphPerformance):
         super().__init__(0, min_precision, max_precision, raw_format)
 
     def _create_output(self, percentiles: {PercentileItem}, title, file_name, output_dir) -> str:
-        return self._create_table(percentiles, title, file_name, output_dir)
-
-    def _create_table(self, percentiles: {PercentileItem}, title, file_name, output_dir):
-        summary_table = PrettyTable()
-
-        percentiles_sort = sorted(list(percentiles.keys()))
-        for label in percentiles[1].executors.keys():
-            table = PrettyTable()
-            table.add_column("Executors", percentiles[1].executors[label])
-            table.add_column("Label", [label]*len(percentiles[1].executors[label]))
-            for percentile in percentiles_sort:
-                suffix = f" {int(percentile * 100)}ph" if percentile < 1 else ""
-                table.add_column(f"Performance{suffix}", percentiles[percentile].total_performance[label])
-            for percentile in percentiles_sort:
-                suffix = f" {int(percentile * 100)}ph" if percentile < 1 else ""
-                table.add_column(f"Avrg{suffix}", percentiles[percentile].avrg_time[label])
-            for percentile in percentiles_sort:
-                suffix = f" {int(percentile * 100)}ph" if percentile < 1 else ""
-                table.add_column(f"Std{suffix}", percentiles[percentile].std_deviation[label])
-
-            if len(summary_table.rows) == 0:
-                summary_table = table
-            else:
-                summary_table.add_rows(table.rows)
-
-        summary_table.border = True
-        summary_table.header = True
-        summary_table.padding_width = 1
-        summary_table.align = "r"
-        summary_table.align["Executors"] = "c"
-        summary_table.align["Label"] = "l"
-
         with open(os.path.join(output_dir, f"TXT-{file_name}.txt"), 'w') as file:
-            file.write(str(summary_table))
+            file.write(str(super()._create_table(percentiles)))
